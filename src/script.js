e0705e26a6310700a85598917a1509ab51ddc386
Marcus <marcus@Marcuss-MBP.hub>
2018-10-05 03:11:44 +1300

    init

---
+++ src/script.js
@@ -0,0 +1,41 @@
+document.getElementById('refreshButton').style.display = 'none';
+
+const puppeteer = require('puppeteer');
+
+const GLASTO_URL = 'https://glastonbury.seetickets.com/';
+let pages = [];
+
+function servePages () {
+    let browsersNumber = document.getElementById("noOfBrowsers").value;
+    
+    if (+browsersNumber) {
+        for(let i=0; i < +browsersNumber; i++) {
+            (async () => {
+                const browser = await puppeteer.launch({
+                        defaultViewport: null,
+                        headless: false
+                    });
+        
+                const page = await browser.newPage();
+                await page.goto(GLASTO_URL);
+                pages.push(page);
+            })();
+        }
+
+        document.getElementById('noOfBrowsersForm').style.display = 'none';
+        document.getElementById('refreshButton').style.display = 'block';
+    }
+}
+
+function refreshPages () {
+    pages.forEach(page => {
+        page.reload();
+    });
+}
+
+var shell = require('electron').shell;
+//open links externally by default
+
+function openLink (link) {
+    shell.openExternal(link);
+}
